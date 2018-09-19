2080ce586cb2fad5bbc8ddb7370f050751c35a3b
Cory Jett <cjett@pivotal.io>
2018-09-19 15:59:35 -0500

    prints request cookie and returns via response cookie

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ var url = require('url');
 var querystring = require('querystring');
 
 http.createServer(function(request, response){
+  console.log(JSON.stringify(request.headers['cookie']));
   response.writeHead(200, {'Content-type':'text/plan',
   'customHeader':'myCustomHeader',
   // 'Set-Cookie':'myCookie=Cookies!',
   // 'Set-Cookie':'JSESSIONID=E7BB55477DB45B2CD3DE81FF394B3FF6'
-  'Set-Cookie': ['myCookie=Cookies!', 'JSESSIONID=E7BB55477DB45B2CD3DE81FF394B3FF6'],
+  'Set-Cookie': ['myCookie=Cookies!', 'JSESSIONID=E7BB55477DB45B2CD3DE81FF394B3FF6', request.headers['cookie']],
 
   });
-  response.write('Hello Node JS Server Response');
+  response.write(request.headers['cookie']);
   response.end( );
 
-   pathName= url.parse(request.url).pathname;
-   query= url.parse(request.url).query;
-   if (!pathName.includes('favicon')){
-   console.log('pathName: ' + pathName);
-   console.log('query: ' + query);
- }
+   // pathName= url.parse(request.url).pathname;
+   // query= url.parse(request.url).query;
+   // if (!pathName.includes('favicon')){
+   // console.log('pathName: ' + pathName);
+   // console.log('query: ' + query);
+ // }
 }).listen(process.env.PORT || 7000);
